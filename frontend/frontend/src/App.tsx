72fa271ef9686e6739810a7018c929a6d7497a5e
Rishik Kumar <rishik.k2002@gmail.com>
2024-07-16 11:38:06 +0530

    BACKGROUND

---
+++ frontend/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Fragment, useState } from 'react';
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='pageContainer'>
+        div here
+      </div>
+    </>
+  )
+}
+
+export default App
