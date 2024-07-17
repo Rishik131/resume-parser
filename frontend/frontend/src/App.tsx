8b0b5453dc95eec1b0d1ca988cb86c35a8bedc5c
Rishik Kumar <rishik.k2002@gmail.com>
2024-07-18 01:53:13 +0530

    file uplaod feature added

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,15 +1,36 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+import { FaCloudUploadAlt } from "react-icons/fa";
 
 function App() {
-  
-
+  const [file, setFile] = useState(null);
+  const handleFileUpload = (event:any) => {
+    setFile(event.target.files[0]);
+  }
   return (
     <>
       <div className='pageContainer'>
         <div className='pageContainerChild'>
           <div className="fileContainer">
-            dsfewf
+            <div className="fileContainerChild1">
+              Upload Your Resume
+            </div>
+            <div className="fileContainerChild2">
+              <div className="uploadContainer">
+                <div className="uploadContainerChild1">
+                  <label htmlFor="inputFile">
+                    <FaCloudUploadAlt className='uploadIcon' />
+                    <input onChange={handleFileUpload} accept='.pdf' type="file" style={{'display':'none'}}  id='inputFile' />
+                  </label>
+                </div>
+                <div className="uploadContainerChild2">
+                  Upload your Resume here
+                </div>
+                <div>
+                  
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
